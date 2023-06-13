22cd86bbec62a5afa4d52d62572da6891da0032d
Pedro Filipe <pedrofilipe92@hotmail.com>
2023-06-13 20:28:43 -0300

    controller

--- routes/web.php
+++ routes/web.php
@@ -13,21 +13,25 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
-
-Route::get('/sobre-nos', function() {
-    return 'Sobre nós';
-});
-
-Route::get('/contato', function() {
-    return 'Contato';
-});
-
 // Route::get($uri, $callback);
 // $uri = rota, caminho de entrada
 // $callback = ação que deve ser tomada quando essa rota for acessada
+// Route::get('/', function () {
+//     return view('welcome');
+// });
+// Route::get('/sobre-nos', function() {
+//     return 'Sobre nós';
+// });
+// Route::get('/contato', function() {
+//     return 'Contato';
+// });
+
+Route::get('/', 'PrincipalController@principal');
+
+Route::get('/sobre-nos', 'SobreNosController@sobreNos');
+
+Route::get('contato', 'ContatoController@contato');
+
 
 // verbos http
 // get
