a80a5d01393eae359e82c06d409eca317aae19d0
Pedro Filipe <pedrofilipe92@hotmail.com>
2023-06-13 20:14:10 -0300

    rotas

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,23 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/sobre-nos', function() {
+    return 'Sobre nós';
+});
+
+Route::get('/contato', function() {
+    return 'Contato';
+});
+
+// Route::get($uri, $callback);
+// $uri = rota, caminho de entrada
+// $callback = ação que deve ser tomada quando essa rota for acessada
+
+// verbos http
+// get
+// post
+// put
+// patch
+// delete
+// options
