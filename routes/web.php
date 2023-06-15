7e255c734f146a713e8499a6cfaf2fd792337b4f
Pedro Filipe <pedrofilipe92@hotmail.com>
2023-06-15 15:17:27 -0300

    parametros opcionais

--- routes/web.php
+++ routes/web.php
@@ -32,8 +32,8 @@ Route::get('/sobre-nos', 'SobreNosController@sobreNos');
 
 Route::get('/contato', 'ContatoController@contato');
 // passando parametros pela url
-Route::get('/contato/{nome}/{categoria}/{assunto}/{mensagem}', 
-    function(string $nome, string $categoria, string $assunto, string $mensagem) {
+Route::get('/contato/{nome?}/{categoria?}/{assunto?}/{mensagem?}', 
+    function(string $nome = '', string $categoria = '', string $assunto = '', string $mensagem = '') {
         echo 'nome: '. $nome . '<br>categoria: ' . $categoria . '<br>assunto: ' . $assunto . '<br>mensagem: ' . $mensagem;
     });
 
