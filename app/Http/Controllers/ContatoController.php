0ddac50504846c513ee931939d5faa748f562f8e
Pedro Filipe <pedrofilipe92@hotmail.com>
2023-06-16 15:49:50 -0300

    envio de formularios via get

--- app/Http/Controllers/ContatoController.php
+++ app/Http/Controllers/ContatoController.php
@@ -7,6 +7,8 @@ use Illuminate\Http\Request;
 class ContatoController extends Controller
 {
     public function contato() {
+        // utilizando a super global get para recuperar os dados do front
+        var_dump($_GET);
         return view('site.contato');
     }
 }
--- resources/views/site/contato.blade.php
+++ resources/views/site/contato.blade.php
@@ -11,21 +11,23 @@
 
     <div class="informacao-pagina">
         <div class="contato-principal">
-            <form>
-                <input type="text" placeholder="Nome" class="borda-preta">
+            {{-- enviando dados do formulario --}}
+            {{-- method get envia os dados pela url --}}
+            <form action={{ route("site.contato") }} method="get">
+                <input name="nome" type="text" placeholder="Nome" class="borda-preta">
                 <br>
-                <input type="text" placeholder="Telefone" class="borda-preta">
+                <input name="telefone" type="text" placeholder="Telefone" class="borda-preta">
                 <br>
-                <input type="text" placeholder="E-mail" class="borda-preta">
+                <input name="email" type="text" placeholder="E-mail" class="borda-preta">
                 <br>
-                <select class="borda-preta">
+                <select name="motivo_contato" class="borda-preta">
                     <option value="">Qual o motivo do contato?</option>
                     <option value="">Dúvida</option>
                     <option value="">Elogio</option>
                     <option value="">Reclamação</option>
                 </select>
                 <br>
-                <textarea class="borda-preta">Preencha aqui a sua mensagem</textarea>
+                <textarea name="mensagem" class="borda-preta">Preencha aqui a sua mensagem</textarea>
                 <br>
                 <button type="submit" class="borda-preta">ENVIAR</button>
             </form>
