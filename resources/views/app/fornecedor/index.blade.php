ba447117b8be6bf2da774ecc69edbe94a354402e
Pedro Filipe <pedrofilipe92@hotmail.com>
2023-06-16 14:12:39 -0300

    for no blade

--- resources/views/app/fornecedor/index.blade.php
+++ resources/views/app/fornecedor/index.blade.php
@@ -54,3 +54,21 @@
             <p>{{ $fornecedores[1]['ddd'] }}</p>
     @endswitch
 @endisset
+
+@for ($i = 0; isset($fornecedores[$i]); $i++)
+    <p>Fornecedor: {{ $fornecedores[$i]['nome'] ?? ''}}</p>
+    <p>Status: {{ $fornecedores[$i]['status'] ?? ''}}</p>
+    <p>Cnpj: {{ $fornecedores[$i]['cnpj'] ?? ''}}</p>
+    <p>Telefone: ({{ $fornecedores[$i]['ddd'] ?? '' }}) {{ $fornecedores[$i]['telefone'] ?? ''}}</p>
+    @switch($fornecedores[$i]['ddd'])
+        @case('21')
+            <p>São Paulo</p>
+            @break
+        @case('71')
+            <p>Salvador</p>
+            @break
+        @default
+            <p>{{ $fornecedores[$i]['ddd'] }}</p>
+    @endswitch
+    <hr>
+@endfor
