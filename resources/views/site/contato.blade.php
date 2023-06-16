04e13011ef7276d1c019fa8e57b153033f2304c4
Pedro Filipe <pedrofilipe92@hotmail.com>
2023-06-16 16:05:12 -0300

    components

--- resources/views/site/contato.blade.php
+++ resources/views/site/contato.blade.php
@@ -11,29 +11,9 @@
 
     <div class="informacao-pagina">
         <div class="contato-principal">
-            {{-- enviando dados do formulario --}}
-            {{-- method get envia os dados pela url --}}
-            {{-- <form action={{ route("site.contato") }} method="get"> --}}
-            <form action={{ route("site.contato") }} method="post">
-                {{-- method post precisa do token --}}
-                @csrf
-                <input name="nome" type="text" placeholder="Nome" class="borda-preta">
-                <br>
-                <input name="telefone" type="text" placeholder="Telefone" class="borda-preta">
-                <br>
-                <input name="email" type="text" placeholder="E-mail" class="borda-preta">
-                <br>
-                <select name="motivo_contato" class="borda-preta">
-                    <option value="">Qual o motivo do contato?</option>
-                    <option value="">Dúvida</option>
-                    <option value="">Elogio</option>
-                    <option value="">Reclamação</option>
-                </select>
-                <br>
-                <textarea name="mensagem" class="borda-preta">Preencha aqui a sua mensagem</textarea>
-                <br>
-                <button type="submit" class="borda-preta">ENVIAR</button>
-            </form>
+            @component('site.layouts._components.form_contato')
+                
+            @endcomponent
         </div>
     </div>  
 </div>
