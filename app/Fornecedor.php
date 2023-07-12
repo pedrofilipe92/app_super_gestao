068e4d911369db13fa4aa36e4b65644ec4fd1a16
Pedro Filipe <pedrofilipe92@hotmail.com>
2023-07-12 13:47:14 -0300

    create e fillable

--- app/Fornecedor.php
+++ app/Fornecedor.php
@@ -8,4 +8,11 @@ class Fornecedor extends Model
 {
     // definindo tabela com o plural correto
     protected $table = 'fornecedores';
+
+    protected $fillable = [
+        'nome',
+        'site',
+        'uf',
+        'email'
+    ];
 }
