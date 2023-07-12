1c1967abecbebb60be8ad79a11d76500ef3a52df
Pedro Filipe <pedrofilipe92@hotmail.com>
2023-07-12 14:37:38 -0300

    fillable em SiteContato

--- app/SiteContato.php
+++ app/SiteContato.php
@@ -6,5 +6,11 @@ use Illuminate\Database\Eloquent\Model;
 
 class SiteContato extends Model
 {
-    //
+    protected $fillable = [
+        'nome',
+        'telefone',
+        'email',
+        'motivo_contato',
+        'mensagem'
+    ];
 }
