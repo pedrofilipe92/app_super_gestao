3f4b76b93974b9de68127dd061c707e7a46f7db5
Pedro Filipe <pedrofilipe92@hotmail.com>
2023-07-13 16:09:57 -0300

    implementando softDelete

--- app/Fornecedor.php
+++ app/Fornecedor.php
@@ -3,9 +3,13 @@
 namespace App;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class Fornecedor extends Model
 {
+    // implementando deleted_at
+    use SoftDeletes;
+
     // definindo tabela com o plural correto
     protected $table = 'fornecedores';
 
--- app/SiteContato.php
+++ app/SiteContato.php
@@ -3,9 +3,13 @@
 namespace App;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class SiteContato extends Model
 {
+    // implementando deleted_at
+    use SoftDeletes;
+
     protected $fillable = [
         'nome',
         'telefone',
