601ec27acad31476dde2d57d0e43a041d671ca08
Pedro Filipe <pedrofilipe92@hotmail.com>
2023-07-20 14:20:32 -0300

    recuperando dados no back com request

--- app/Http/Controllers/ContatoController.php
+++ app/Http/Controllers/ContatoController.php
@@ -6,11 +6,18 @@ use Illuminate\Http\Request;
 
 class ContatoController extends Controller
 {
-    public function contato() {
+    public function contato(Request $request) {
         // utilizando a super global get para recuperar os dados do front
         // var_dump($_GET);
         // utilizando a super global post para recuperar os dados do front
-        var_dump($_POST);
+        // var_dump($_POST);
+
+        echo '<pre>';
+        // recupera todo o conteudo
+        print_r($request->all());
+        echo '</pre>';
+        // recupera o atributo desejado
+        echo $request->input('telefone');
         return view('site.contato');
     }
 }
--- resources/views/site/layouts/_components/form_contato.blade.php
+++ resources/views/site/layouts/_components/form_contato.blade.php
@@ -13,10 +13,10 @@
     <input name="email" type="text" placeholder="E-mail" class="{{ $classe }}">
     <br>
     <select name="motivo_contato" class="{{ $classe }}">
-        <option value="">Qual o motivo do contato?</option>
-        <option value="">Dúvida</option>
-        <option value="">Elogio</option>
-        <option value="">Reclamação</option>
+        <option value="0">Qual o motivo do contato?</option>
+        <option value="1">Dúvida</option>
+        <option value="2">Elogio</option>
+        <option value="3">Reclamação</option>
     </select>
     <br>
     <textarea name="mensagem" class="{{ $classe }}">Preencha aqui a sua mensagem</textarea>
