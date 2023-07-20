b545bc8ff87c5bd73876b178f644d5dd0f8093ee
Pedro Filipe <pedrofilipe92@hotmail.com>
2023-07-20 17:49:49 -0300

    tratando erros no front campo a campo

--- resources/views/site/layouts/_components/form_contato.blade.php
+++ resources/views/site/layouts/_components/form_contato.blade.php
@@ -7,10 +7,19 @@
     {{-- method post precisa do token --}}
     @csrf
     <input name="nome" value="{{ old('nome') }}" type="text" placeholder="Nome" class="{{ $classe }}">
+    @if($errors->has('nome'))
+        {{ $errors->first('nome') }}
+    @endif
     <br>
     <input name="telefone" value="{{ old('telefone') }}" type="text" placeholder="Telefone" class="{{ $classe }}">
+    @if($errors->has('telefone'))
+        {{ $errors->first('telefone') }}
+    @endif
     <br>
     <input name="email" value="{{ old('email') }}" type="text" placeholder="E-mail" class="{{ $classe }}">
+    @if($errors->has('email'))
+        {{ $errors->first('email') }}
+    @endif
     <br>
     <select name="motivo_contatos_id" class="{{ $classe }}">
         <option value="">Qual o motivo do contato?</option>
@@ -21,20 +30,26 @@
         <option value="2" {{ old('motivo_contato' == 2) ? 'selected' : '' }}>Elogio</option>
         <option value="3" {{ old('motivo_contato' == 3) ? 'selected' : '' }}>Reclamação</option> --}}
     </select>
+    @if($errors->has('motivo_contatos_id'))
+        {{ $errors->first('motivo_contatos_id') }}
+    @endif
     <br>
     <textarea name="mensagem" class="{{ $classe }}">
         {{ old('mensagem') != '' ? old('mensagem') : 'Preencha aqui a sua mensagem' }}
     </textarea>
+    @if($errors->has('mensagem'))
+        {{ $errors->first('mensagem') }}
+    @endif
     <br>
     <button type="submit" class="{{ $classe }}">ENVIAR</button>
 </form>
 
 {{-- tratando os erros no front --}}
-@if($errors->any())
+{{-- @if($errors->any())
     <div style="position:absolute; top:0px; left:0px; width:100%; background:red">
         @foreach($errors->all() as $erro)
             {{ print_r($erro) }}
             <br>
         @endforeach
     </div>
-@endif
+@endif --}}
