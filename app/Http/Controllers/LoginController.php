29641acf156afa570c81cedb9d189a8c1c963d92
Pedro Filipe <pedrofilipe92@hotmail.com>
2023-07-21 16:49:20 -0300

    implementando logout

--- app/Http/Controllers/LoginController.php
+++ app/Http/Controllers/LoginController.php
@@ -49,6 +49,7 @@ class LoginController extends Controller
     }
 
     public function sair() {
-        echo 'sair';
+        session_destroy();
+        return redirect()->route('site.index');
     }
 }
