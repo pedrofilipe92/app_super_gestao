29ad97041b12f9f8d8d6deaccbaffc689a592bf8
Pedro Filipe <pedrofilipe92@hotmail.com>
2023-07-26 17:03:47 -0300

    create com validacao usando resource

--- app/Http/Controllers/ProdutoController.php
+++ app/Http/Controllers/ProdutoController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use App\Produto;
+use App\Unidade;
 use Illuminate\Http\Request;
 
 class ProdutoController extends Controller
@@ -30,7 +31,8 @@ class ProdutoController extends Controller
      */
     public function create()
     {
-        echo 'create';
+        $unidades = Unidade::all();
+        return view('app.produto.create', ['unidades' => $unidades]);
     }
 
     /**
@@ -41,7 +43,29 @@ class ProdutoController extends Controller
      */
     public function store(Request $request)
     {
-        //
+        $sucesso = '';
+        $regras = [
+            'nome'          => 'required|min:3|max:40',
+            'descricao'     => 'max:200',
+            'peso'          => 'required|integer',
+            // precisa existir na tabela unidades
+            // '<variavel>' => 'exists:<tabela>,<coluna>'
+            'unidade_id'    => 'exists:unidades,id'
+        ];
+        $mensagens = [
+            'required'  => 'O campo :attribute é obrigatório.',
+            'integer'   => 'O campo :attribute deve ser um inteiro.',
+            'min'       => 'O campo :attribute deve ter no mínimo :min caracteres.',
+            'max'       => 'O campo :attribute deve ter no máximo :max caracteres.',
+            'exists'    => 'Unidade não cadastrada.'
+        ];
+
+        $request->validate($regras, $mensagens);
+
+        Produto::create($request->all());
+        $sucesso = 'Produto incluído com sucesso.';
+
+        return redirect()->route('produto.index', ['sucesso' => $sucesso]);
     }
 
     /**
--- resources/views/app/produto/index.blade.php
+++ resources/views/app/produto/index.blade.php
@@ -19,6 +19,7 @@
         </div>
         <div class="informacao-pagina">
             <div style="width: 90%; margin-left: auto; margin-right: auto;">
+                {{ $sucesso ?? '' }}
                 <table border="1" width="100%">
                     <thead>
                         <tr>
